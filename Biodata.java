b7f7d23933ffaf18680304fcc66e34363aaaf6a1
gesang144 <44185920+gesang144@users.noreply.github.com>
2019-09-17 18:35:01 +0700

    biodata saya

---
+++ Biodata.java
@@ -0,0 +1,22 @@
+public class BiodataSaya {
+
+    public static void main(String[] args) {
+
+        String nama;
+        Integer umur;
+        String alamat;
+
+        nama = "GesangPrayoga";
+        umur = 18;
+        alamat = "Ds.Buduran Kec.Wonoasri Kab.Madiun";
+
+        final String tanggalLahir = "14 November 2000";
+
+
+        System.out.println("Nama Saya Adalah " + nama);
+        System.out.println("Saya Berumur " + umur + " Tahun Dan Beralamat " + alamat);
+        System.out.println("Selain Itu, Saya Lahir Pada Tanggal " + tanggalLahir);
+
+
+    }
+}
